657ab1f56ca0bd8a9b345e0060553b482ede5d42
MR.IBAY <ibayjimwell@gmail.com>
2025-01-12 16:05:32 +0800

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -48,4 +48,4 @@ def serve_static(path):
         return send_from_directory(app.static_folder, 'index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
